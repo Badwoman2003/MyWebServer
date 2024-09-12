f9164850bf06e76cfd2185bd278dae81441b0431
Badwoman <3242204470@qq.com>
2024-09-12 23:11:20 +0800

    add log moduler

--- code/log/log.h
+++ code/log/log.h
@@ -1,13 +1,55 @@
 #pragma once
 
-#include<mutex>
-#include<string>
-#include<thread>
-#include<sys/time.h>
-#include<string.h>
-#include<stdarg.h>
-#include<assert.h>
-#include<sys/stat.h>
-#include"blockqueue.h"
-#include"../buffer/Buffer.h"
+#include <mutex>
+#include <string>
+#include <thread>
+#include <sys/time.h>
+#include <string.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include "blockqueue.h"
+#include "../buffer/Buffer.h"
 
+class Log
+{
+private:
+    Log(/* args */);
+    virtual ~Log();
+    void AppeendLogLevelTitle_(int level);
+    void AsyncWrite_(); // async write log
+public:
+    void init(int level, const char *path = "./log", const char *suffix = ".log", int maxQueueCapacity = 1024);
+    static Log *instance();
+    static void FlushLogThread();
+
+    void write(int level, const char *format, ...);
+    void flush();
+
+    int GetLevel();
+    void SetLevel(int level);
+    bool IsOpen() { return isOpen_; };
+
+private:
+    static const int LOG_PATH_LEN = 256;
+    static const int LOG_NAME_LEN = 256;
+    static const int MAX_LINES = 5000;
+
+    const char *path_;
+    const char *suffix_;
+
+    int Max_Lines_;
+
+    int Line_count_;
+    int toDay_;
+
+    bool isOpen_;
+
+    Buffer buff_;
+    int level_;
+    bool isAsync_;
+
+    FILE* fp_;
+    std::unique_ptr<BlockQueue<std::string>> deque_;
+    std::unique_ptr<std::thread> writTread_;
+};
