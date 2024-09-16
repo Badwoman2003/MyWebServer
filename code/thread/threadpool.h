4dc0ebc6e09f88a34b8e67bef6b3fa9236d6bd78
Badwoman <3242204470@qq.com>
2024-09-16 22:09:48 +0800

    add class ThreadPool

--- code/thread/threadpool.h
+++ code/thread/threadpool.h
@@ -1,3 +1,30 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include <thread>
+#include<assert.h>
+
+class ThreadPool
+{
+private:
+    struct Pool
+    {
+        std::mutex mtx;
+        std::condition_variable cond_;
+        bool isClosed;
+        std::queue<std::function<void()>> tasks;
+    };
+    std::shared_ptr<Pool> pool;
+    
+public:
+    ThreadPool() = default;
+
+    ~ThreadPool();
+};
+
+
+
 #endif
