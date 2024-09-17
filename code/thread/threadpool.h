0dcb2062136255282921b307b9c9689444b26698
Badwoman <3242204470@qq.com>
2024-09-17 23:18:51 +0800

    finished threadpool define

--- code/thread/threadpool.h
+++ code/thread/threadpool.h
@@ -5,7 +5,7 @@
 #include <condition_variable>
 #include <functional>
 #include <thread>
-#include<assert.h>
+#include <assert.h>
 
 class ThreadPool
 {
@@ -17,14 +17,57 @@ private:
         bool isClosed;
         std::queue<std::function<void()>> tasks;
     };
-    std::shared_ptr<Pool> pool;
-    
+    std::shared_ptr<Pool> pool_;
+
 public:
     ThreadPool() = default;
+    ThreadPool(ThreadPool &&t) = default;
+    explicit ThreadPool(int threadCount = 8) : pool_(std::shared_ptr<Pool>())
+    {
+        assert(threadCount > 0);
+        for (int i = 0; i < threadCount; i++)
+        {
+            std::thread([this]()
+                        {
+                            std::unique_lock<std::mutex> locker(pool_->mtx);
+                            while (true)
+                            {
+                                if (!pool_->tasks.empty())
+                                {
+                                    auto task = std::move(pool_->tasks.front());
+                                    pool_->tasks.pop();
+                                    task();
+                                    locker.lock();
+                                }
+                                else if (pool_->isClosed)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    pool_->cond_.wait(locker);
+                                }
+                            } })
+                .detach();
+        }
+    }
+    ~ThreadPool()
+    {
+        if (pool_)
+        {
+            std::unique_lock<std::mutex> locker(pool_->mtx);
+            pool_->isClosed = true;
+        }
+        pool_->cond_.notify_all();
+    };
 
-    ~ThreadPool();
+    template <typename T>
+    void AddTask(T &&task)
+    {
+        std::unique_lock<std::mutex> locker(pool_->mtx);
+        pool_->tasks.emplace(std::forward<T>(task));
+        pool_->cond_.notify_one();
+    }
 };
-
-
 
 #endif
