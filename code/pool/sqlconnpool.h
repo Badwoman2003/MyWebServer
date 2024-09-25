a1d02a0e4ca3bd2b75550de054fc8bce2da4f779
Badwoman <3242204470@qq.com>
2024-09-25 23:29:23 +0800

    update sqlconnpool

--- code/pool/sqlconnpool.h
+++ code/pool/sqlconnpool.h
@@ -1,4 +1,34 @@
 #ifndef SQLCONNPOOL_H
 #define SQLCONNPOOL_H
 
+#include <mysql/mysql.h>
+#include <string>
+#include <queue>
+#include <mutex>
+#include <semaphore.h>
+#include <thread>
+#include "../log/log.h"
+
+class SqlConnPool
+{
+private:
+    SqlConnPool(/* args */);
+    ~SqlConnPool();
+
+    int MaxConn_;
+    int UseConn_;
+    int FreeConn_;
+
+    std::queue<MYSQL *> conn_queue;
+    std::mutex mtx_;
+    sem_t semId_;
+public:
+    static SqlConnPool* Instance();
+    MYSQL* GetConn();
+    void FreeConn(MYSQL* Conn);
+    int GetFreeConnCount();
+};
+
+
+
 #endif
